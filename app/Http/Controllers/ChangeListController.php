9944676f600e7e7c999f7a2756b2446c7dae4d3b
Jader Moura <jader@aswti.com.br>
2021-05-07 14:18:55 -0300

    dev

--- app/Http/Controllers/ChangeListController.php
+++ app/Http/Controllers/ChangeListController.php
@@ -11,9 +11,27 @@ class ChangeListController extends Controller
     {
 
         $teste = new OrderMonthService();
-        // dd($teste);
+        // dd($teste
         $chk =  (array)$teste->ChangeList();
 
-        dd($chk[0]['janeiro']['alimentos']);
+        // dd($chk[0]['janeiro']['alimentos']);
+        //$arr = $chk[0]['janeiro']['alimentos'];
+        $arr = $chk;
+
+
+        uasort(
+            $arr,
+            function ($value1, $value2) use ($arr) {
+                //dd($value1, $value2, 'aqui');
+                //return strcmp($value1['janeiro'], $value2[1]);
+                //return array_search($value1, $jader) > array_search($value2, $jader);
+
+                //return strcmp(array_search($value1, $tt), array_search($value2, $tt));
+                return  array_search($value2, $arr) < array_search($value1, $arr);
+                //return  sort($t, SORT_NUMERIC);
+            }
+        );
+
+        dd($arr);
     }
 }
